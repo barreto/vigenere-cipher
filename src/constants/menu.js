3f8c0d6ef67932d3783d6dc5a096b6a22b2c1964
Barreto <v.barreto.and@gmail.com>
2021-08-26 23:33:57 -0300

    feat: create contants

---
+++ src/constants/menu.js
@@ -0,0 +1,8 @@
+const menu = `
+    1 - Criptografar
+    2 - Decriptografar
+
+    0 - Sair
+`;
+
+module.exports = menu;
---
+++ src/constants/options.js
@@ -0,0 +1,7 @@
+const options = {
+  cypherOption: "1",
+  decypherOption: "2",
+  quit: "0",
+};
+
+module.exports = options;
